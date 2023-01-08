d5e61b2df0c34dea319b4669a9de16c0de188b5c
SHOMANS <mohammedshoman9@gmail.com>
2023-01-08 11:19:43 +0200

    start session 9

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import Form from './pages/Form';
 import Home from './pages/Home';
-import Counters from './pages/Counters';
-import TodoList from './pages/TodoList';
-import Post from './pages/Post';
-import { Routes, Route, Link, useRoutes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { posts } from './mock/posts';
-import TodoDetails from './pages/TodoDetails';
 
 function App() {
   const router = useRoutes([
     { path: '/', element: <Home /> },
     { path: '/form', element: <Form /> },
-    { path: '/counters', element: <Counters /> },
-    { path: '/todo', element: <TodoList /> },
-    { path: '/todo/:id', element: <TodoDetails /> },
-    { path: '/new', element: <h1>New Page</h1> },
-    {
-      path: '/posts',
-      element: (
-        <ul>
-          {posts.map((item) => (
-            <li key={item.id}>
-              <Link to={`/posts/${item.id}`}>Post {item.id}</Link>
-            </li>
-          ))}
-        </ul>
-      ),
-    },
-    {
-      path: '/posts/:id',
-      element: <Post />,
-    },
     { path: '*', element: <h1>page not found 404</h1> },
   ]);
 
@@ -40,29 +15,6 @@ function App() {
     <div className='App'>
       <Header myPage='home' />
       {router}
-
-      {/* <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/form' element={<Form />} />
-        <Route path='/counters' element={<Counters />} />
-        <Route path='/todo' element={<TodoList />} />
-        <Route path='/new' element={<h1>New Page</h1>} />
-
-        <Route
-          path='/posts'
-          element={
-            <ul>
-              {posts.map((item) => (
-                <li>
-                  <Link to={`/posts/${item.id}`}>Post {item.id}</Link>
-                </li>
-              ))}
-            </ul>
-          }
-        />
-        <Route path='/posts/:id' element={<Post />} />
-        <Route path='*' element={<h1>page not found 404</h1>} />
-      </Routes> */}
 
       <Footer />
     </div>
