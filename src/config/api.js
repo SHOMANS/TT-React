4a3ff7fa7ce0337f38dd0876bae4b7b049a3fdd2
SHOMANS <mohammedshoman9@gmail.com>
2023-01-10 14:14:45 +0200

    finish session 10

---
+++ src/config/api.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://react-tt-api.onrender.com/api';
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -2,14 +2,53 @@ import React from 'react';
 import Container from '../components/Container';
 
 class InfoPage extends React.Component {
-  state = {
-    isMounted: true,
-  };
+  constructor(props) {
+    super(props);
+    this.state = { myState: false, counter: 0 };
+    console.log('from constructor');
+  }
+
+  componentDidMount() {
+    console.log('from did mount');
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('from get derived');
+    return { myState: props.state };
+  }
+
+  shouldComponentUpdate() {
+    console.log('from should component update');
+    if (this.state.counter > 5 && this.state.counter < 10) {
+      return false;
+    }
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('from get snapshot');
+    // console.log(prevState.counter);
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log('from did update');
+  }
+
+  componentWillUnmount() {
+    console.log('from will unmount');
+  }
+
   render() {
+    console.log('from render');
+    // console.log(this.state.counter);
+
     return (
       <div>
         <Container className='my-container'>
           <h2>Info Page</h2>
+          <h3>counter: {this.state.counter}</h3>
+          <button onClick={() => this.setState((prevState) => ({ counter: prevState.counter + 1 }))}>click</button>
         </Container>
       </div>
     );
